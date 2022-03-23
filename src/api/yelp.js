3d98f6cf3d7ebafb142e216586b2bdc6743d1940
Gerardo Martinez <31676475+jerrym7@users.noreply.github.com>
2022-03-23 12:56:00 -0700

    Update yelp.js

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
-        Authorization: 'Bearer AM0TVo6PziBl5iB8nfEuWTaAbumaawgTulMJLxkWhDdG19gwG4itzEgQvt4jO7TohN-5nHhKUgCKFfrWGBQpGtGYB54U_gCj__ZvMpBrZLXPyEkPYol9__jTijI6YnYx'
+        Authorization: 'Bearer API_KEY_HERE'
     }
 });
 //call it 
